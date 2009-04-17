43c87bd70d7b54f2ca06396905abc873b92e1002
gsoftware@free.fr <gsoftware@free.fr@c81cc944-0b1d-11de-9c5c-7db7966a26d7>
2009-04-17 20:22:09 +0000

    git-svn-id: http://django-graphviz.googlecode.com/svn/trunk@9 c81cc944-0b1d-11de-9c5c-7db7966a26d7

--- graphviz/admin.py
+++ graphviz/admin.py
@@ -10,12 +10,14 @@ admin.site.register(Graph, GraphAdmin)
 class NodeVisualAdmin(admin.ModelAdmin):
    list_display = ('name', 'shape', 'options', 'graph', 'content_type')
    list_filter = ('content_type', 'graph')
+   save_as = True
 admin.site.register(NodeVisual, NodeVisualAdmin)
 
 
 class ArrowVisualAdmin(admin.ModelAdmin):
    list_display = ('shape', 'modifier', 'graph', 'content_type')
    list_filter = ('content_type', 'graph')
+   save_as = True
 admin.site.register(ArrowVisual, ArrowVisualAdmin)
 
 

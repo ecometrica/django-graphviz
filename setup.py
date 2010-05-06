936cb095cbe6d281d8483dc3704b9878c36de056
Eric St-Jean <esj@wwd.ca>
2010-05-06 11:18:20 -0400

    added setup.py to make this pip- and easy_install-installable

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-graphviz',
+    version='0.1',
+    description='Django Graphviz extension',
+    author='gsoftware@free.fr',
+    author_email='gsoftware@free.fr',
+    url='http://code.google.com/p/django-graphviz/',
+    packages=['graphviz'],
+    package_data={'graphviz': ['*.dot']},
+    zip_safe=False,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: End Users/Desktop', 
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Database'
+    ]
+)

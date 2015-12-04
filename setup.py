a85f16219471ee5ab29ea8f6ff17cd6ef64f1869
mghughes <mghughes@users.noreply.github.com>
2015-12-04 14:32:04 -0400

    Add `graphviz.migrations` to packages in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='gsoftware@free.fr',
     author_email='gsoftware@free.fr',
     url='http://code.google.com/p/django-graphviz/',
-    packages=['graphviz', 'graphviz.management', 'graphviz.management.commands'],
+    packages=['graphviz', 'graphviz.management', 'graphviz.management.commands', 'graphviz.migrations'],
     package_data={'graphviz': ['graphviz/templates/graphviz/*.dot']},
     zip_safe=False,
     classifiers = [
